3f79650e81caac10cae0c6a1c7257811e78e9c9f
viktoralyoshin <viktor.aleshin2004@gmail.com>
2025-04-25 15:34:40 +0300

    init project

---
+++ src/main/java/com/nookly/book/NooklyApplication.java
@@ -0,0 +1,13 @@
+package com.nookly.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NooklyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NooklyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nookly/book/NooklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nookly.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NooklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
